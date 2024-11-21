cd1913c138d7715b358e1492c7fbb1426ab5ed19
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-11-22 00:44:21 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,13 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import "./index.css"
-// import { GoogleOAuthProvider } from "@react-oauth/google";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-//       <App />
-//     </GoogleOAuthProvider>
-//   </React.StrictMode>
-// );
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import App from './App.jsx';
-import './index.css';
-
-// Load the Google Client ID from environment variables
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-
-if (!clientId) {
-  throw new Error('Google Client ID is not defined');
-}
-
-console.log('Google Client ID:', clientId); // Debugging: Check if Client ID is loaded
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <GoogleOAuthProvider clientId={clientId}>
-        <App />
-      </GoogleOAuthProvider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
-
