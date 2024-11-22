e2812c62fbe174d916c0f5c34477befa2624cda4
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-11-22 16:21:08 +0530

    main.jsx changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css"
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>
-);
-
-////22-11-2024
-
 // import React from "react";
 // import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";
+// import App from "./App.jsx";
 // import "./index.css"
 // import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // ReactDOM.createRoot(document.getElementById("root")).render(
 //   <React.StrictMode>
-//     <BrowserRouter>
-//       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-//         <App />
-//       </GoogleOAuthProvider>
-//     </BrowserRouter>
+//     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+//       <App />
+//     </GoogleOAuthProvider>
 //   </React.StrictMode>
 // );
+
+////22-11-2024
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css"
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <App />
+      </GoogleOAuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
